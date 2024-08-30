7c6fc6117117357b24debe0ed2138832dd6d9d2e
NATASHA <klimknatasha@yandex.ru>
2024-08-30 18:08:48 +0300

    1

---
+++ 2.c
@@ -0,0 +1,13 @@
+/* Задание 2. Вывести на экран текст "лесенкой"
+Let’s
+  go
+   to walk
+*/
+
+#include <stdio.h> 
+
+int main(void) { 
+
+   printf("Let\'s \n\t go \n\t\t to walk"); 
+   return 0;
+}
